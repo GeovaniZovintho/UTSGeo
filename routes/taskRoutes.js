fb67458ab5ff03c60000c7fb637f92675b653e4b
GeovaniZovintho <geovanizovintho@gmail.com>
2025-02-25 13:08:54 +0700

    first commit

---
+++ routes/taskRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const taskController = require('../controllers/taskController');
+const { authMiddleware, validateTask } = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.post('/tasks',validateTask, taskController.createTask);
+router.get('/tasks', taskController.getTasks);
+router.put('/tasks/:id', taskController.updateTask);
+router.delete('/tasks/:id', taskController.deleteTask);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const WebSocket = require("ws");
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+const wss = new WebSocket.Server({ port: 8080 });
+wss.on("connection", (ws) => {
+    console.log("🔗 Client terhubung");
+    ws.send("Selamat datang di notifikasi tugas!");
+});
+
+app.use(express.json());
+app.use('/auth', authRoutes);
+app.use('/', taskRoutes);
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
